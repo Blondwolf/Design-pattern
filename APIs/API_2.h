3f043dfdc52ca1ff5314ef4e1dd895bd96a06e4d
Arkeine <arkeine@Mjolnir>
2015-03-09 14:33:39 +0100

    adding codeblock project with bridge and the API classes

---
+++ APIs/API_2.h
@@ -0,0 +1,19 @@
+#ifndef API_2_h
+#define API_2_h
+
+
+class API_2 {
+
+ public:
+
+    static void drawRect(Integer x, Integer y, Integer l, Integer w, Integer a);
+
+    static void drawExagon(Integer x, Integer y, Integer r, Integer a);
+
+    static void drawCircle(Integer x, Integer y, Integer r);
+
+ public:
+    Integer newAttr;
+};
+
+#endif // API_2_h
