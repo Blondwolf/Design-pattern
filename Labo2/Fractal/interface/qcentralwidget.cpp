e532ae82a1f4065c764d73886e5e3d4daab671e6
Arkeine <arkeine@Mjolnir>
2015-03-27 09:46:40 +0100

    recherche et destruction du bug de Raphael

--- Labo2/Fractal/interface/qcentralwidget.cpp
+++ Labo2/Fractal/interface/qcentralwidget.cpp
@@ -8,12 +8,6 @@ QCentralWidget::QCentralWidget(QWidget *parent):QWidget(parent)
     QHBoxLayout *h= new QHBoxLayout(this);
     h->addWidget(qpw);
     h->addWidget(qdw);
-    QGroupBox *g= new QGroupBox(this);
-    g->resize(800,600);
-    g->setMaximumSize(800,600);
-    g->setMinimumSize(800,600);
-    g->show();
-
 }
 
 QCentralWidget::~QCentralWidget()
--- Labo2/Fractal/main.cpp
+++ Labo2/Fractal/main.cpp
@@ -5,13 +5,12 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-//    MainWindow w;
-//    w.resize(800,600);
-//    w.setMinimumSize(800,600);
-//    w.setMaximumSize(800,600);
-//    w.show();
-QParametreWidget w;
-w.show();
+    MainWindow w;
+    w.resize(800,600);
+    w.setMinimumSize(800,600);
+    w.setMaximumSize(800,600);
+    w.show();
+
 
     return a.exec();
 }
